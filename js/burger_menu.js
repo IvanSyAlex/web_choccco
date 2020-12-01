eff3376d85d001e0789dd8b082716c29cccd3b70
IvanSyAlex <protonunet@gmail.com>
2020-12-02 00:05:28 +0500

    slider & burger menu

---
+++ js/burger_menu.js
@@ -0,0 +1,41 @@
+const openHamburger = document.querySelector('.hamburger');
+const closeHamberger = document.querySelector('.menu--hamburger--close')
+const menu = document.querySelector('.menu');
+const menu_list = document.querySelector('.menu__list');
+const menu_item = document.querySelectorAll('.menu__item');
+
+function openMenu(e){
+    e.preventDefault();
+    menu.classList.toggle('menu--hamburger--active');
+    closeHamberger.style.right =  42 +'px';
+    closeHamberger.style.opacity = 1;
+    menu_list.classList.toggle('menu__list--hamburger--active');
+    for(let i=0; i<menu_item.length; i++){
+        menu_item[i].classList.toggle('menu__item--hamburger--active');
+    } 
+}
+
+function closeMenu(){
+    menu.classList.remove('menu--hamburger--active');
+    menu_list.classList.remove('menu__list--hamburger--active');
+    closeHamberger.style.right =  -9999 +'px';
+    closeHamberger.style.opacity = 0;
+    for(let i=0; i<menu_item.length; i++){
+       menu_item[i].classList.remove('menu__item--hamburger--active');
+    }  
+}
+openHamburger.addEventListener('click', e => openMenu(e));//открыть меню
+closeHamberger.addEventListener('click', closeMenu);//закрыть меню
+
+menu.addEventListener('click', e => {
+    if(e.target==menu){
+        menu.classList.remove('menu--hamburger--active');
+        menu_list.classList.remove('menu__list--hamburger--active');
+        closeHamberger.style.right =  -9999 +'px';
+        closeHamberger.style.opacity = 0;
+        for(let i=0; i<menu_item.length; i++){
+            menu_item[i].classList.remove('menu__item--hamburger--active');
+        }  
+    }
+ 
+});
